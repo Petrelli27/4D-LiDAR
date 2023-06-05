83ce1eadb357a24136e733e333ce7780fc0b4c9d
Peter Li <peterljy317@gmail.com>
2023-06-05 17:31:58 -0400

    orientation association

--- analyze2.py
+++ analyze2.py
@@ -3,6 +3,7 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial.transform import Rotation
+from scipy import linalg
 import math
 import lidarScan2
 import boundingbox
@@ -73,7 +74,10 @@ H_0 = 2  # Initial Height of box - z
 p_0 = [-200., -200., -50.]#np.array(debris_pos[0])  # Guess of initial position of debris - *need to formulate guess*
 v_0 = [1., 1., 1.]#np.array(debris_vel[0])  # Initial guess of relative velocity of debris, can be based on how fast plan to approach during rendezvous
 omega_0 = [5.,-5.,4.] #np.array(omega_L)  # Initial guess of angular velocities - *need to formulate guess*
-
+q_0 = [1.,0.,0.,0.] # initialize orientation as a unit quaternion
+p1_0 = p_0 + np.array([L_0/2, -D_0/2, -H_0/2]) 
+x_0 = np.array([p_0, v_0, omega_0, p1_0, q_0]).ravel()
+x_k = x_0.copy()  # State vector
 for i in range(nframes):
     # Decompose the state vector
     p_k = x_k[:3]
@@ -105,19 +109,55 @@ for i in range(nframes):
     #######################
     # Make measurements
     #######################
+    PLs.append((linalg.inv(Rot_L_to_B[i]) @ (PBs[i]).T).T)
+    # find bounding box from points
+    XLs.append(PLs[i][:, 0])
+    YLs.append(PLs[i][:, 1])
+    ZLs.append(PLs[i][:, 2])
+    X_i = XLs[i]
+    Y_i = YLs[i]
+    Z_i = ZLs[i]
 
     # find bounding box from points
 
     # Return bounding box and centroid estimate of bounding box
+    z_pi_k, z_p_k = boundingbox.bbox3d(X_i, Y_i, Z_i)
 
     # Orientation association
+    # R_1 is obtained from bounding box
+    predicted_R = Rotation.from_quat(q_kp1)
+    possible_Rs = []
+    angle_diffs = []
+    possible_xs = np.array([[1,0,0],[0,1,0],[0,0,1],[-1,0,0],[0,-1,0],[0,0,-1]])
+    possible_ys = np.array([[1,0,0],[0,1,0],[0,0,1],[-1,0,0],[0,-1,0],[0,0,-1]])
+    for x_axis in possible_xs:
+        for y_axis in possible_ys:
+            if (x_axis == y_axis).all():
+                continue # skip over case where x and y axis overlap
+            else:
+                z_axis = np.cross(x_axis, y_axis)
+                possible_R = np.array(x_axis, y_axis, z_axis)
+                possible_Rs.append(possible_R)
+                rotation_diff = (predicted_R.T) @ (possible_R)
+                angle_diff = np.arccos((np.trace(possible_R)-1)/2)
+                angle_diffs.append(angle_diff)
+    R_index = np.argmin(np.abs(angle_diffs))
+    associated_R = possible_Rs(R_index)
+    z_q_k = Rotation.as_quat(associated_R)
+    
 
     # find angular velocity from LOS velocities
     # 1. Linear Least Squares
+    z_omega_k_B = estimate(XBs[i], YBs[i], ZBs[i], Rot_L_to_B[i]@p_k, Rot_L_to_B[i]@v_k, VBs[i])
+    z_omega_k = Rot_B_to_L[i] @ z_omega_k_B
     # 2. Rotation of B Frame
     # 3. Kabsch
 
-    # Get Measurement Vector
+    # Compute Measurement Vector
+
+    ##############
+    # Combine Measurement and Estimates
+    ##############
 
     # Calculate Kalman gain
 
@@ -131,4 +171,7 @@ for i in range(nframes):
 
     # Append for analysis
 
+    ##############
+    # Plot relevant figures
+    ##############
     # Plot relevant figures
