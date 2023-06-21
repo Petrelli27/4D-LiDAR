rotation fix maybe

associationdata.py
@@ -106,7 +106,7 @@ def rotation_association(q_kp1, R_1):
     possible_ys = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [-1, 0, 0], [0, -1, 0], [0, 0, -1]])
     for x_axis in possible_xs:
         for y_axis in possible_ys:
-            if (x_axis == y_axis).all():
+            if (x_axis == y_axis).all() or (np.cross(x_axis, y_axis)==0).all():
                 continue  # skip over case where x and y axis overlap
             else:
                 z_axis = np.cross(x_axis, y_axis)

boundingbox.py
@@ -36,8 +36,8 @@ def bbox3d(x, y, z, return_evec=False):
     means = np.mean(data, axis=1)
     cov = np.cov(data)
     #print(cov)
-    eval, evec = LA.eig(cov)
-
+    # evals, evec = LA.eig(cov)
+    evec, s, vh = LA.svd(cov)
     centered_data = data - means[:, np.newaxis]
 
     # Bounding box based on simple max min (not rotation compensated)
@@ -126,7 +126,7 @@ def bbox3d(x, y, z, return_evec=False):
 
     #ax.scatter(c_x, c_y, c_z, color='b', linewidth=4)
     if return_evec:
-        return rrc, [c_x,c_y,c_z], evec
+        return rrc, [c_x,c_y,c_z], evec.T
     else:
         return rrc, [c_x,c_y,c_z]
     